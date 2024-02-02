6917046a70b9f875c3692d0586cc772827f4ae71
u gayashan <80288026+u-gayashan@users.noreply.github.com>
2024-02-03 03:03:16 +0530

    Update llm_response_repository.py

--- app/repositories/llm_response_repository.py
+++ app/repositories/llm_response_repository.py
@@ -16,16 +16,20 @@ from pgvector.psycopg2 import register_vector
 from langchain.vectorstores.pgvector import PGVector
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 
+import google.generativeai as genai
+import googletrans
+from googletrans import Translator
+
 class LLMRepository:
     def __init__(self):
         self.sys = ""
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
         
         
-        model_kwargs = {"device": "cpu"}
-        encode_kwargs = {"normalize_embeddings": True}
+        self.model_kwargs = {"device": "cpu"}
+        self.encode_kwargs = {"normalize_embeddings": True}
 
-        embeddings = HuggingFaceBgeEmbeddings(
+        self.embeddings = HuggingFaceBgeEmbeddings(
             model_name="physician-ai/Model81", model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
         )
         
@@ -40,7 +44,10 @@ class LLMRepository:
         #     model_name="hkunlp/instructor-large", model_kwargs={"device": self.device}
         # )
         
-        # self.new_db = FAISS.load_local("../utils/faiss_index", embeddings)
+        genai.configure(api_key="AIzaSyAjG_p_DA8rSsTNUt1w4zQ_7MIZ9ADqvqk")
+        gemini_model = genai.GenerativeModel('gemini-pro')
+
+        self.new_db = FAISS.load_local("../utils/faiss_index", self.embeddings)
 
         self.model_name_or_path = "TheBloke/Llama-2-13B-chat-GPTQ"
         self.model_basename = "model"
@@ -92,7 +99,7 @@ class LLMRepository:
         self.qa_chain = RetrievalQA.from_chain_type(
             llm=self.llm,
             chain_type="stuff",
-            retriever=store.as_retriever(search_kwargs={"k": 2}),
+            retriever=self.store.as_retriever(search_kwargs={"k": 2}),
             return_source_documents=True,
             chain_type_kwargs={"prompt": self.prompt},
         )
@@ -100,11 +107,14 @@ class LLMRepository:
         self.qa_chain_a = RetrievalQA.from_chain_type(
             llm=self.llm2,
             chain_type="stuff",
-            retriever=store.as_retriever(search_kwargs={"k": 2}),
+            retriever=self.store.as_retriever(search_kwargs={"k": 2}),
             return_source_documents=True,
             chain_type_kwargs={"prompt": self.prompt},
         )
-
+        
+    def translate(text,language):
+        return str(translator.translate(text,dest=keys[vals.index(language)]).text)
+        
     def generate_prompt(self, prompt=None, system_prompt=None):
         if prompt is None:
             prompt = """
@@ -122,6 +132,36 @@ class LLMRepository:
         {prompt} [/INST]
         """.strip()
 
+    def QA():
+        print("\nopen QA mode running ========================================\n")
+
+        try:
+            print("\n google gemini===================\n")
+            gemini_chat = gemini_model.start_chat(history=[])
+            
+            gemini_response = gemini_chat.send_message('give next small response for laste patient response like a doctor.  '+str(chain))
+            return gemini_response.text
+        except:
+            print("\n llmmaa ===================\n")
+            result_ex = qa_chain(sys+chain+"""\n\n\nalways give small and single response based on the patient 
+                response. don't ask any question give simple response""")
+            if "Patient:" in str(result_ex['result']) or "Patient response:" in str(result_ex['result']) or "Patient Response" in str(result_ex['result']):
+                return str((str(result_ex['result']).split("\n\n")[-1]).split(":")[-1])
+            else:
+                try:
+                   return str(result_ex['result']).split("\n\n")[1]
+                except:
+                   return str(result_ex['result']).split("\n\n")
+    if str(mode)=="dirrect_QA" and id==3:
+        diagnosis_and_treatment = qa_chain(sys+chain+end_sys_prompts)
+        diagnosis_and_treatment = str(diagnosis_and_treatment['result'])
+        print(diagnosis_and_treatment)
+        print("dirrect answer")
+        return {"english":diagnosis_and_treatment,"translated":translate(diagnosis_and_treatment,language)}
+    if str(mode)=="dirrect_QA" and id>3:
+        qa_text =  str(QA())
+        return {"english":qa_text,"translated":translate(qa_text,language)}
+    
     def refresh_model(self):
         self.llm = HuggingFacePipeline(pipeline=self.text_pipeline, model_kwargs={"temperature": 2})
         self.llm2 = HuggingFacePipeline(pipeline=self.text_pipeline, model_kwargs={"temperature": 2})
@@ -129,7 +169,7 @@ class LLMRepository:
         self.qa_chain = RetrievalQA.from_chain_type(
             llm=self.llm,
             chain_type="stuff",
-            retriever=store.as_retriever(search_kwargs={"k": 2}),
+            retriever=self.store.as_retriever(search_kwargs={"k": 2}),
             return_source_documents=True,
             chain_type_kwargs={"prompt": self.prompt},
         )
@@ -137,7 +177,7 @@ class LLMRepository:
         self.qa_chain_a = RetrievalQA.from_chain_type(
             llm=self.llm2,
             chain_type="stuff",
-            retriever=store.as_retriever(search_kwargs={"k": 2}),
+            retriever=self.store.as_retriever(search_kwargs={"k": 2}),
             return_source_documents=True,
             chain_type_kwargs={"prompt": self.prompt},
         )
@@ -145,75 +185,46 @@ class LLMRepository:
         print("Model refreshed")
         
     def get_response(self, chain, id, mode):
-        if id < 13:
-            if id >= 8:
-                return self.final_question[id]
+        if id<13:
+
+            if id>=8:
+                return {"english":final_question[id],"translated":translate(final_question[id],language)}
             else:
-                if id < 5:
-                    question = self.qa_chain(
-                        self.sys + chain + """
-                    \n\nask single small queston to get details based on the patient response,and don't ask 
+                if id<5:
+                    # 2 . prompmt control the natural way on question asking based on patient response,symptomps type
+                    question = qa_chain(sys+chain +"""\n\nask single small queston to get details based on the patient response,and don't ask 
                     same question again, and don't provide treatment and diagnosis ask next small and short question , 
-                    always don't ask same question again and again , always only ask next single small question"""
-                    )
+                    always don't ask same question again and again , always only ask next single small question""")
+
                 else:
-                    question = self.qa_chain(
-                        self.sys + chain + """
-                    \n\nask single small queston to get details based on the patient response,and don't ask 
+                    # 3. prompt will guide the model to ask yes or no questions based on patient response,symptomps type
+                    question = qa_chain(sys+chain +"""\n\nask single small queston to get details based on the patient response,and don't ask 
                     same question again, and don't provide treatment and diagnosis ask next small and short question with yes or no format , 
-                    always don't ask same question again and again , always only ask next single small question"""
-                    )
+                    always don't ask same question again and again , always only ask next single small question""")
                 try:
-                    if (
-                        "Patient:" in str(question["result"])
-                        or "Patient response:" in str(question["result"])
-                        or "Patient Response" in str(question["result"])
-                    ):
-                        return str((str(question["result"]).split("\n\n")[-1]).split(":")[-1])
+                    if "Patient:" in str(question['result']) or "Patient response:" in str(question['result']) or "Patient Response" in str(question['result']):
+
+                        return {"english":str((str(question['result']).split("\n\n")[-1]).split(":")[-1]),"translated":translate(str((str(question['result']).split("\n\n")[-1]).split(":")[-1]),language)}
                     else:
-                        return str(question["result"]).split("\n\n")[1]
+
+                        return {"english":str(question['result']).split("\n\n")[1],"translated":translate(str(question['result']).split("\n\n")[1],language)}
 
                 except:
-                    if (
-                        "Patient:" in str(question["result"])
-                        or "Patient response:" in str(question["result"])
-                        or "Patient Response" in str(question["result"])
-                    ):
-                        return str(question["result"]).split(":")[-1]
+                    if "Patient:" in str(question['result']) or "Patient response:" in str(question['result']) or "Patient Response" in str(question['result']):
+
+                       return {"english":str(question['result']).split(":")[-1],"translated":translate(str(question['result']).split(":")[-1],language)}
                     else:
-                        return str(question["result"])
 
-        if id == 13:
-            end_sys_prompts = "\n\ngive correct treatment and most related diagnosis with ICD code don't ask any questions. if question is not related to provided data don't give answer from this provided data's"
-            diagnosis_and_treatment = self.qa_chain(self.sys + chain + self.end_sys_prompts)
-            diagnosis_and_treatment = str(diagnosis_and_treatment["result"])
-            print(mode)
-            if str(mode) == "dirrect":
-                print(diagnosis_and_treatment)
-                print("dirrect answer")
-                return diagnosis_and_treatment
+                       return {"english":str(question['result']),"translated":translate(str(question['result']),language)}
 
-            else:
-                report = self.qa_chain_a(
-                    self.report_prompt_template
-                    + self.sys
-                    + chain
-                    + "\n\ntreatment & diagnosis with ICD code below\n"
-                    + diagnosis_and_treatment
-                )
-                print(str(report["result"]))
-                print("h&P")
-                return str(report["result"])
+        if id==13:
+            diagnosis_and_treatment = qa_chain(sys+chain+end_sys_prompts)
+            diagnosis_and_treatment = str(diagnosis_and_treatment['result'])
+            print(mode,diagnosis_and_treatment)
+            report = qa_chain_a(report_prompt_template+sys+chain+"\n\ntreatment & diagnosis with ICD code below\n"+diagnosis_and_treatment)
+            print(str(report['result']))
+            print("h&P")
+            return {"english":str(report['result']),"translated":translate(str(report['result']),language)}
 
-        result_ex = self.qa_chain(
-            self.sys + chain + """\n\n\nalways give small and single response based on the patient 
-            response. don't give multiline response always give response based on last patient response"""
-        )
-        if (
-            "Patient:" in str(result_ex["result"])
-            or "Patient response:" in str(result_ex["result"])
-            or "Patient Response" in str(result_ex["result"])
-        ):
-            return str((str(result_ex["result"]).split("\n\n")[-1]).split(":")[-1])
-        else:
-            return str(result_ex["result"]).split("\n\n")[1]
+        qa_text =  str(QA())        
+        return {"english":qa_text,"translated":translate(qa_text,language)}
